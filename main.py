fce3dbc2eb906ef5c6853c63566404ffcbe57304
Kyle Scher <kms3218@rit.edu>
2025-09-30 16:16:59 -0400

    added point vis. I believe there to be a frame sync issue

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from src.hal.cam.depth import get_static_depth, disparity_to_points
 from src.hal.cam.calib import load_calibration
 
 import cv2, time, numpy as np
-
+import open3d as o3d
+import matplotlib.pyplot as plt
 
 def main():
-
+    time.sleep(15)
     calib = load_calibration()
     disp, vis = get_static_depth(show=False)
     if disp is None:
@@ -31,6 +32,25 @@ def main():
     print("Click on the depth map to see 3D coordinates. Press any key to exit.")
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+    mask = np.isfinite(points[:, :, 2]) & (points[:, :, 2] > 0)
+    pts = points[mask]
+    cols = vis[mask] / 255.0
+
+    # Always build the Open3D object
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(pts)
+    pcd.colors = o3d.utility.Vector3dVector(cols)
+
+    print(f"Point cloud has {len(pts)} points")
+
+    print("Falling back to Matplotlib scatter plot...")
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    # downsample for speed
+    sample = pts[::500]
+    ax.scatter(sample[:, 0], sample[:, 1], sample[:, 2], s=1)
+    plt.show()
 
 
 if __name__ == "__main__":
